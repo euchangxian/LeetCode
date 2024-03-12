7e1b24e091a4775f24b41bc834c066276f0db1b1
euchangxian <changxian2001@gmail.com>
2024-03-12 22:33:52 +0800

    feat/122-BestTimeToBuyAndSellStockTwo
    Visualising the prices array as a graph, where simply getting the
    difference of every adjacent element on a increasing slope will give the
    solution/maxProfit.

---
+++ 122-BestTimeToBuyAndSellStockTwo/Solution_test.go
@@ -0,0 +1,38 @@
+package lc122
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMaxProfit(t *testing.T) {
+	testCases := map[string]struct {
+		prices   []int
+		expected int
+	}{
+		"Buy and sell twice - day 2, day 3 and day 4, day 5": {
+			prices:   []int{7, 1, 5, 3, 6, 4},
+			expected: 7,
+		},
+		"Non-decreasing prices - buy once on day 1, sell on day 5": {
+			prices:   []int{1, 2, 3, 4, 5},
+			expected: 4,
+		},
+		"Non-increasing prices - never buy": {
+			prices:   []int{7, 6, 4, 3, 1},
+			expected: 0,
+		},
+	}
+
+	for name, tc := range testCases {
+		// Capture loop variables
+		name := name
+		tc := tc
+		t.Run(name, func(t *testing.T) {
+			t.Parallel()
+			actual := maxProfit(tc.prices)
+			assert.Equal(t, tc.expected, actual)
+		})
+	}
+}
