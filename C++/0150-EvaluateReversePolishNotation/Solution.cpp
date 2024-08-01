0349dfbe5f6330036e7a1f4c832e56f16799d378
euchangxian <e0957388@u.nus.edu>
2024-08-01 22:34:08 +0800

    feat(C++): Add 150

---
+++ C++/0150-EvaluateReversePolishNotation/Solution.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+private:
+  unordered_map<string, function<int(int, int)>> operations = {
+      {"+", plus<>()},
+      {"-", minus<>()},
+      {"*", multiplies<>()},
+      {"/", divides<>()}};
+
+public:
+  int evalRPN(vector<string> &tokens) {
+    stack<int> operands;
+
+    for (string const &token : tokens) {
+      if (operations.count(token)) {
+        // Qn constraint means the equation is valid
+        int op2 = operands.top();
+        operands.pop();
+        int op1 = operands.top();
+        operands.pop();
+
+        int result = operations[token](op1, op2);
+        operands.push(result);
+        continue;
+      }
+
+      operands.push(stoi(token));
+    }
+    return operands.top();
+  }
+};
