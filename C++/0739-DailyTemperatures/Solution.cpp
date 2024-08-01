2d37f4fcf895e782064b6b699b855a2672bc0262
euchangxian <e0957388@u.nus.edu>
2024-08-02 00:05:27 +0800

    feat(C++): Add 739
    Also solved already. This time it feels rather simple.

---
+++ C++/0739-DailyTemperatures/Solution.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<int> dailyTemperatures(vector<int> &temperatures) {
+    vector<int> result(temperatures.size(), 0);
+
+    // Monotonically non-increasing stack
+    stack<int> indices;
+    for (int i = 0; i < temperatures.size(); ++i) {
+      while (!indices.empty() &&
+             temperatures[i] > temperatures[indices.top()]) {
+        int top = indices.top();
+        indices.pop();
+
+        result[top] = i - top;
+      }
+
+      indices.push(i);
+    }
+    return result;
+  }
+};
