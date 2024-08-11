85b96202f84f56762dd93438d6955a805234b480
euchangxian <changxian2001@gmail.com>
2024-08-11 20:05:11 +0800

    feat(C++): Add 5
    Turns out I have not done this question!

--- C++/0005-LongestPalindromicSubstring/Solution.cpp
+++ C++/0005-LongestPalindromicSubstring/Solution.cpp
@@ -10,6 +10,36 @@
 
 using namespace std;
 class Solution {
+private:
+  pair<int, int> expandCentre(const string &s, int left, int right) {
+    while (left >= 0 && right < s.length() && s[left] == s[right]) {
+      --left;
+      ++right;
+    }
+    // Move left/right indices back to compensate for last iteration
+    return {left + 1, right - 1};
+  }
+
 public:
-  string longestPalindrome(string s) {}
+  string longestPalindrome(string s) {
+    int n = s.length();
+
+    int start = 0;
+    int maxLength = 1;
+    for (int i = 0; i < n; ++i) {
+      const auto &[oddL, oddR] = expandCentre(s, i, i);
+      const auto &[evenL, evenR] = expandCentre(s, i, i + 1);
+
+      if (oddR - oddL + 1 > maxLength) {
+        start = oddL;
+        maxLength = oddR - oddL + 1;
+      }
+
+      if (evenR - evenL + 1 > maxLength) {
+        start = evenL;
+        maxLength = evenR - evenL + 1;
+      }
+    }
+    return s.substr(start, maxLength);
+  }
 };
