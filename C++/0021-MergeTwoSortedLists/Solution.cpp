96dd45dc3be824f49d7f2cfefcb9b0cfdf02b438
euchangxian <e0957388@u.nus.edu>
2024-08-13 21:49:45 +0800

    feat(C++): Add 21
    A little bit more practice with wishful thinking

---
+++ C++/0021-MergeTwoSortedLists/Solution.cpp
@@ -0,0 +1,39 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+using namespace std;
+class Solution {
+private:
+public:
+  ListNode *mergeTwoLists(ListNode *list1, ListNode *list2) {
+    if (list1 == nullptr) {
+      return list2;
+    }
+    if (list2 == nullptr) {
+      return list1;
+    }
+
+    if (list1->val <= list2->val) {
+      list1->next = mergeTwoLists(list1->next, list2);
+      return list1;
+    } else {
+      list2->next = mergeTwoLists(list1, list2->next);
+      return list2;
+    }
+  }
+};
