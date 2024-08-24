77db26c8bb64c1f0ef2201e3852257a5d2b09102
euchangxian <changxian2001@gmail.com>
2024-08-25 02:44:17 +0800

    feat(C++): Add 102

---
+++ C++/0102-BinaryTreeLevelOrderTraversal/Solution.cpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+public:
+  vector<vector<int>> levelOrder(TreeNode *root) {
+    if (!root) {
+      return {};
+    }
+    // BFS
+    vector<vector<int>> result;
+    queue<vector<TreeNode *>> frontiers;
+    frontiers.push({root});
+    while (!frontiers.empty()) {
+      vector<TreeNode *> frontier = frontiers.front();
+      frontiers.pop();
+
+      vector<int> level;
+      vector<TreeNode *> next;
+      for (TreeNode *&node : frontier) {
+        level.push_back(node->val);
+
+        if (node->left) {
+          next.push_back(node->left);
+        }
+        if (node->right) {
+          next.push_back(node->right);
+        }
+      }
+
+      result.push_back(std::move(level));
+
+      if (!next.empty()) {
+        frontiers.push(std::move(next));
+      }
+    }
+    return result;
+  }
+};
