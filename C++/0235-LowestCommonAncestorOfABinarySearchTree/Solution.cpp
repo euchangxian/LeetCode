6b42862a79f433c5120e2ae45a97179347beafe3
euchangxian <changxian2001@gmail.com>
2024-08-25 02:30:33 +0800

    feat(C++): Add 235
    Subtly different from 236: LCA of Binary Tree
    
    Similar in concept

---
+++ C++/0235-LowestCommonAncestorOfABinarySearchTree/Solution.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+public:
+  TreeNode *lowestCommonAncestor(TreeNode *root, TreeNode *p, TreeNode *q) {
+    // Exploit the sorted property of a BST
+    // O(logn) assuming the BST is balanced
+    // Question constraints guarantee that p and q exists. Therefore, there
+    // must be an LCA which will never be null
+    if (p->val < root->val && q->val < root->val) {
+      // Both p and q lie in the left subtree
+      return lowestCommonAncestor(root->left, p, q);
+    }
+
+    if (p->val > root->val && q->val > root->val) {
+      // Both p and q lie in the right subtree
+      return lowestCommonAncestor(root->right, p, q);
+    }
+
+    // Split found
+    return root;
+  }
+
+  TreeNode *lowestCommonAncestorNaive(TreeNode *root, TreeNode *p,
+                                      TreeNode *q) {
+    // Does not make use of the sorted property of a BST
+    // O(n)
+    if (!root) {
+      return nullptr;
+    }
+
+    if (root == p || root == q) {
+      return root;
+    }
+
+    TreeNode *left = lowestCommonAncestor(root->left, p, q);
+    TreeNode *right = lowestCommonAncestor(root->right, p, q);
+
+    if (left && right) {
+      // root is the LCA
+      return root;
+    }
+
+    if (left) {
+      // Left root is the LCA
+      return left;
+    }
+
+    return right;
+  }
+};
