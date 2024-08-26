a19b9c93897ba38833b307c3e8ccfe969dc3c9cf
euchangxian <e0957388@u.nus.edu>
2024-08-26 18:14:43 +0800

    feat(C++): Add 1046

---
+++ C++/1046-LastStoneWeight/Solution.cpp
@@ -0,0 +1,41 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int lastStoneWeight(vector<int> &stones) {
+    // In each round, choose the two heaviest stone and smash them together
+    // That means we need a max heap
+    priority_queue<int, vector<int>, less<int>> pq;
+
+    for (auto const &stone : stones) {
+      pq.push(stone);
+    }
+
+    while (pq.size() > 1) {
+      int y = pq.top();
+      pq.pop();
+      int x = pq.top();
+      pq.pop();
+
+      if (y - x) {
+        pq.push(y - x);
+      }
+    }
+
+    if (pq.empty()) {
+      return 0;
+    }
+
+    return pq.top();
+  }
+};
