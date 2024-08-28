57f92a1044e30d1ff5af30e2229a0fa96128cc4c
euchangxian <changxian2001@gmail.com>
2024-08-29 00:11:56 +0800

    feat(C++): Add 190

---
+++ C++/0190-ReverseBits/Solution.cpp
@@ -0,0 +1,24 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  uint32_t reverseBits(uint32_t n) {
+    int result = 0;
+
+    for (int i = 0; i < 32; ++i) {
+      // Left shift result, then add the right-most bit of n
+      result = (result << 1) + ((n >> i) & 1);
+    }
+    return result;
+  }
+};
