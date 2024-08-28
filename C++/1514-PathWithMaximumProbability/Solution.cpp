a6a1705430b3fc4696386fb56fabda52022c30ea
euchangxian <e0957388@u.nus.edu>
2024-08-28 11:58:27 +0800

    feat(C++): Add 1514
    SSSP. To review

---
+++ C++/1514-PathWithMaximumProbability/Solution.cpp
@@ -0,0 +1,18 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  double maxProbability(int n, vector<vector<int>> &edges,
+                        vector<double> &succProb, int start_node,
+                        int end_node) {}
+};
