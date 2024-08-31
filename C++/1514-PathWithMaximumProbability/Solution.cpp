22eeaac0405ed7420a0052eb6e644e88fc37321b
euchangxian <changxian2001@gmail.com>
2024-08-31 16:37:21 +0800

    chore(C++): Refactor 1514

--- C++/1514-PathWithMaximumProbability/Solution.cpp
+++ C++/1514-PathWithMaximumProbability/Solution.cpp
@@ -18,35 +18,37 @@ public:
     // Reduce to an SSSP problem.
     // For multiplication, to convert to a sum, intuition should be logarithms
     // To find the maximum probability, negate the edge weights.
-    vector<vector<pair<int, double>>> modifiedEdges(n);
+    vector<vector<pair<double, int>>> modifiedEdges(n);
     for (int i = 0; i < edges.size(); ++i) {
       int from = edges[i][0];
       int to = edges[i][1];
 
       double weight = succProb[i];
 
-      modifiedEdges[from].push_back({to, weight});
-      modifiedEdges[to].push_back({from, weight});
+      modifiedEdges[from].emplace_back(weight, to);
+      modifiedEdges[to].emplace_back(weight, from);
     }
 
     vector<double> distances(n, 0);
     distances[start_node] = 1.0;
 
-    priority_queue<pair<double, int>> pq;
-    pq.push({1.0, start_node});
+    // max heap instead of usual min heap to pop the greatest probability first
+    priority_queue<pair<double, int>, vector<pair<double, int>>,
+                   less<pair<double, int>>>
+        pq;
+    pq.emplace(1.0, start_node);
 
     while (!pq.empty()) {
       auto [currProb, currNode] = pq.top();
       pq.pop();
 
-      for (auto const &[to, prob] : modifiedEdges[currNode]) {
+      for (auto const &[prob, to] : modifiedEdges[currNode]) {
         if (distances[to] < currProb * prob) {
           distances[to] = currProb * prob;
-          pq.push({distances[to], to});
+          pq.emplace(distances[to], to);
         }
       }
     }
     return distances[end_node];
   }
 };
-
