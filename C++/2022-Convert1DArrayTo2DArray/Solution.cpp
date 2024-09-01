97fe7adbb03a8fe7afc0465955247a6464499a08
euchangxian <changxian2001@gmail.com>
2024-09-01 16:47:48 +0800

    feat(C++): Add 2022
    Trivial question after working on so many Union Find questions that
    involved mapping 2D indices to 1D indices.

---
+++ C++/2022-Convert1DArrayTo2DArray/Solution.cpp
@@ -0,0 +1,29 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<vector<int>> construct2DArray(vector<int> &original, int m, int n) {
+    if (original.size() != m * n) {
+      return {};
+    }
+
+    vector<vector<int>> grid(m, vector<int>(n, 0));
+    for (int i = 0; i < m; ++i) {
+      for (int j = 0; j < n; ++j) {
+        int mappedIdx = i * n + j;
+        grid[i][j] = original[mappedIdx];
+      }
+    }
+    return grid;
+  }
+};
