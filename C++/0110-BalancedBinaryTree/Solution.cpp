08344cbc0914c6fb6bd2fc5526656deb28f8fe16
euchangxian <changxian2001@gmail.com>
2024-09-02 23:26:56 +0800

    feat(C++): Add 110

---
+++ C++/0110-BalancedBinaryTree/Solution.cpp
@@ -0,0 +1,47 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+private:
+  int height(TreeNode *curr) {
+    if (!curr) {
+      return 0;
+    }
+
+    int leftHeight = height(curr->left);
+    int rightHeight = height(curr->right);
+
+    if (leftHeight == -1 || rightHeight == -1 ||
+        abs(leftHeight - rightHeight) > 1) {
+      return -1;
+    }
+
+    return max(leftHeight, rightHeight) + 1;
+  }
+
+public:
+  bool isBalanced(TreeNode *root) {
+    int result = height(root);
+
+    return result != -1;
+  }
+};
