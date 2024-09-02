f9f2cc72e8a1d7343c21a0940844cd03882729c2
euchangxian <e0957388@u.nus.edu>
2024-09-02 12:44:45 +0800

    feat(C++): Add 1894
    Did not feel like a Medium, more of an Easy.

---
+++ C++/1894-FindTheStudentThatWillReplaceTheChalk/Solution.cpp
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int chalkReplacer(vector<int> &chalk, int k) {
+    long long sum = reduce(chalk.begin(), chalk.end(), 0LL, plus<>{});
+
+    // remaining
+    k = (k % sum);
+    for (int i = 0; i < chalk.size(); ++i) {
+      k -= chalk[i];
+      if (k < 0) {
+        return i;
+      }
+    }
+    return -1; // Should not reach this point
+  }
+};
