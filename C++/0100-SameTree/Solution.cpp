50a75d8c3206e5f790af7235f7031fceb5ec2b53
euchangxian <changxian2001@gmail.com>
2024-09-02 23:32:45 +0800

    feat(C++): Add 100

---
+++ C++/0100-SameTree/Solution.cpp
@@ -0,0 +1,37 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+public:
+  bool isSameTree(TreeNode *p, TreeNode *q) {
+    if (!p && q || p && !q) {
+      return false;
+    }
+
+    if (!p && !q) {
+      return true;
+    }
+
+    return (p->val == q->val) && isSameTree(p->left, q->left) &&
+           isSameTree(p->right, q->right);
+  }
+};
