079bd2a2d313d5fb5ecb302c106bb6f1570d7032
euchangxian <changxian2001@gmail.com>
2024-09-02 21:34:45 +0800

    feat(C++): Add 104

---
+++ C++/0104-MaximumDepthOfBinaryTree/Solution.cpp
@@ -0,0 +1,32 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+public:
+  int maxDepth(TreeNode *root) {
+    if (!root) {
+      return 0;
+    }
+
+    return max(maxDepth(root->left), maxDepth(root->right)) + 1;
+  }
+};
