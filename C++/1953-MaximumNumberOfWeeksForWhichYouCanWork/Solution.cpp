349c1e82b3ecc787473575320b881b86fb26dfb6
euchangxian <changxian2001@gmail.com>
2024-09-07 21:31:36 +0800

    feat(C++): Add 1953
    These Greedy problems involve a little bit of math

---
+++ C++/1953-MaximumNumberOfWeeksForWhichYouCanWork/Solution.cpp
@@ -0,0 +1,34 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  long long numberOfWeeks(vector<int> &milestones) {
+    int n = milestones.size();
+
+    // Suppose that the most milestones is 10. The subsequent have 9 milestones.
+    // Regardless of the rest, the number of weeks that can be done without
+    // violating the conditions will be limited by the most milestones,
+    // and is represented by the total sum of milestones (not including the
+    // most), multiplied by 2, add 1 since the mostMilestones can be done 1
+    // more time.
+    int mostMilestones = *max_element(milestones.begin(), milestones.end());
+
+    long long totalMilestones =
+        reduce(milestones.begin(), milestones.end(), 0LL, plus<>{});
+    long long otherMilestones =
+        totalMilestones - static_cast<long long>(mostMilestones);
+
+    return min(totalMilestones, otherMilestones * 2 + 1);
+  }
+};
