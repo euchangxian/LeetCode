b1183b613337223756488c2858d634ad23ed5f5f
euchangxian <changxian2001@gmail.com>
2024-09-07 23:22:49 +0800

    feat(C++): Add 55

---
+++ C++/0055-JumpGame/Solution.cpp
@@ -0,0 +1,35 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  bool canJump(vector<int> &nums) {
+    int n = nums.size();
+    int jumps = 1;
+    int furthest = nums[0]; // Represents the furthest index reachable
+    for (int i = 1; i < n; ++i) {
+      if (i > furthest) {
+        return false;
+      }
+
+      if (i + nums[i] > furthest) {
+        furthest = i + nums[i];
+      }
+
+      if (furthest >= n - 1) {
+        break;
+      }
+    }
+    return true;
+  }
+};
