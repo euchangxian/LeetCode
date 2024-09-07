b4a7de757e299275d27e4bb6c611ef9930d6a6c1
euchangxian <changxian2001@gmail.com>
2024-09-07 14:09:47 +0800

    feat(C++): Add 1367
    Similar to isSubTree

---
+++ C++/1367-LinkedListInBinaryTree/Solution.cpp
@@ -0,0 +1,63 @@
+#include <algorithm>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+struct TreeNode {
+  int val;
+  TreeNode *left;
+  TreeNode *right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode *left, TreeNode *right)
+      : val(x), left(left), right(right) {}
+};
+
+struct ListNode {
+  int val;
+  ListNode *next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+using namespace std;
+class Solution {
+private:
+  bool isSame(ListNode *head, TreeNode *root) {
+    if (!head) {
+      return true;
+    }
+    if (!root) {
+      return false;
+    }
+
+    if (head->val != root->val) {
+      return false;
+    }
+
+    return isSame(head->next, root->left) || isSame(head->next, root->right);
+  }
+
+public:
+  bool isSubPath(ListNode *head, TreeNode *root) {
+    if (!head) {
+      return true;
+    }
+    if (!root) {
+      return false;
+    }
+
+    if (isSame(head, root)) {
+      return true;
+    }
+
+    return isSubPath(head, root->left) || isSubPath(head, root->right);
+  }
+};
