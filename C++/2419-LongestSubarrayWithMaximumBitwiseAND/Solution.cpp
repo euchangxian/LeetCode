5dd7b936fc57c332ec2ebd549f7fe8dc23c3d8b8
euchangxian <changxian2001@gmail.com>
2024-09-14 15:24:14 +0800

    feat(C++): Add 2419

---
+++ C++/2419-LongestSubarrayWithMaximumBitwiseAND/Solution.cpp
@@ -0,0 +1,42 @@
+#include <algorithm>
+#include <array>
+#include <climits>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  int longestSubarray(vector<int> const &nums) {
+    // The bitwise AND of two numbers is always smaller than both.
+    // Therefore, find the maximum element. Find the longest contiguous
+    // subarray with that maximum element.
+    int maxElem = 0;
+    int maxSize = 0;
+    int currContiguous = 0;
+    for (int const &num : nums) {
+      if (num > maxElem) {
+        maxElem = num;
+        currContiguous = 0;
+
+        // New maximum found, previous contiguous subarray not valid.
+        maxSize = 0;
+      }
+
+      if (num == maxElem) {
+        ++currContiguous;
+      } else {
+        currContiguous = 0;
+      }
+
+      maxSize = max(maxSize, currContiguous);
+    }
+    return maxSize;
+  }
+};
