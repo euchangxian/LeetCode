e804868fcfc0d2b35284a511bb113bf6a3b6ad40
euchangxian <changxian2001@gmail.com>
2024-09-27 12:40:11 +0800

    feat(C++): Add 1634

---
+++ C++/1634-AddTwoPolynomialsRepresentedAsLinkedLists/Solution.cpp
@@ -0,0 +1,39 @@
+struct PolyNode {
+  int coefficient, power;
+  PolyNode *next;
+  PolyNode() : coefficient(0), power(0), next(nullptr){};
+  PolyNode(int x, int y) : coefficient(x), power(y), next(nullptr){};
+  PolyNode(int x, int y, PolyNode *next)
+      : coefficient(x), power(y), next(next){};
+};
+
+class Solution {
+public:
+  PolyNode *addPoly(PolyNode *poly1, PolyNode *poly2) {
+    if (!poly1) {
+      return poly2;
+    }
+
+    if (!poly2) {
+      return poly1;
+    }
+
+    if (poly1->power == poly2->power) {
+      poly1->coefficient += poly2->coefficient;
+      poly1->next = addPoly(poly1->next, poly2->next);
+      if (poly1->coefficient == 0) {
+        return poly1->next;
+      }
+
+      return poly1;
+    }
+
+    if (poly1->power > poly2->power) {
+      poly1->next = addPoly(poly1->next, poly2);
+      return poly1;
+    }
+
+    poly2->next = addPoly(poly1, poly2->next);
+    return poly2;
+  }
+};
