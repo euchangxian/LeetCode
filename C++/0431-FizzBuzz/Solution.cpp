33743469a85e49120ac682db1505e4b303e1a934
euchangxian <changxian2001@gmail.com>
2024-09-27 11:48:23 +0800

    feat(C++): Add 431
    Pretty challenging

---
+++ C++/0431-FizzBuzz/Solution.cpp
@@ -0,0 +1,27 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  vector<string> fizzBuzz(int n) {
+    std::vector<string> result;
+    result.reserve(n);
+    for (int i = 1; i <= n; ++i) {
+      std::string output;
+      output.reserve(8);
+      if (i % 3 == 0) {
+        output += "Fizz";
+      }
+      if (i % 5 == 0) {
+        output += "Buzz";
+      }
+
+      if (output.empty()) {
+        output += std::to_string(i);
+      }
+      result.push_back(output);
+    }
+    return result;
+  }
+};
