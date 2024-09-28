d32e9f52ece4facacc16f85e3fa2e93e6bcf6e05
euchangxian <changxian2001@gmail.com>
2024-09-28 22:07:31 +0800

    feat(C++): Add 713

---
+++ C++/0713-SubarrayProductLessThanK/Solution.cpp
@@ -0,0 +1,26 @@
+#include <cstdint>
+#include <vector>
+
+using std::vector;
+class Solution {
+public:
+  int numSubarrayProductLessThanK(vector<int> &nums, int k) {
+    if (k <= 1) {
+      return 0;
+    }
+
+    int32_t result{0};
+    int32_t prod{1};
+    size_t l{0};
+    for (size_t r{0}; r < nums.size(); ++r) {
+      prod *= nums[r];
+
+      while (prod >= k) {
+        prod /= nums[l++];
+      }
+
+      result += (r - l + 1);
+    }
+    return result;
+  }
+};
