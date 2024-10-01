d09709a6c27b80f8f010081024609ea7ecc04d92
euchangxian <changxian2001@gmail.com>
2024-10-01 16:02:08 +0800

    feat(C++): Add 31
    Nope. Don't understand permutations.

---
+++ C++/0031-NextPermutation/Solution.cpp
@@ -0,0 +1,51 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cstdint>
+#include <functional>
+#include <iostream>
+#include <queue>
+#include <stack>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+using namespace std;
+class Solution {
+public:
+  void nextPermutation(vector<int> &nums) {
+    // std::next_permutation(nums.begin(), nums.end());
+    // Notice that if the array is already sorted in descending order, no next
+    // larger permutation is possible. Circle back.
+    // Therefore, we need to find the first pair of numbers from the right,
+    // nums[i] and nums[i-1] such that and nums[i] > nums[i-1].
+    // 1 2 3
+    // 1 3 2
+    // 2 1 3
+    // 2 3 1
+    // 3 1 2
+    // 3 2 1
+    int i = nums.size() - 2;
+    while (i >= 0 && nums[i] >= nums[i + 1]) {
+      // Find the first non-decreasing (increasing) element
+      --i;
+    }
+
+    // if i is less than 0, that means that the array is in descending order.
+    // Reverse. Otherwise, find the first nums[j], i < j such that
+    // nums[i] < nums[j] and nums[i] > nums[j+1]. I.e. the next element
+    if (i >= 0) {
+      int j = nums.size() - 1;
+
+      while (nums[i] >= nums[j]) {
+        --j;
+      }
+
+      std::swap(nums[i], nums[j]);
+    }
+    reverse(nums.begin() + i + 1, nums.end());
+  }
+};
