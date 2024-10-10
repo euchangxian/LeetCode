2c6324c4baf3a4abf3e7974e5ef2fadf7dd202ce
euchangxian <changxian2001@gmail.com>
2024-10-10 23:19:49 +0800

    chore(script): Update script to insert IDs without leading zeros

--- update_readme.sh
+++ update_readme.sh
@@ -58,8 +58,7 @@ update_table() {
   in_table && /^\| *:?-+:? *\| *:?-+:? *\| *:?-+:? *\|/ { print; next }
   in_table && /^\|/ {
     split($0, a, "|")
-    gsub(/^[ \t]+|[ \t]+$/, "", a[2])  # Trim whitespace
-    if (a[2] == num) {
+    if (a[2] + 0 == num) {
       if (index($0, icon) == 0) {  # If icon not present, add it
         gsub(/\|[ \t]*$/, "", $0)  # Remove trailing |
         printf("%s %s |\n", $0, icon)
@@ -67,7 +66,7 @@ update_table() {
         print $0
       }
       updated = 1
-    } else if (a[2] > num && !updated) {
+    } else if ((a[2] + 0 > num) && !updated) {
       printf("| %s | %s | %s |\n", num, name, icon)
       print $0
       updated = 1
@@ -96,7 +95,7 @@ update_table() {
 # Process changed files
 echo "$CHANGED_SOLUTIONS" | while read -r file; do
   if [[ $file =~ ([^/]+)/([0-9]{4})-([^/]+)/Solution\.(.+)$ ]]; then
-    NUMBER=${BASH_REMATCH[2]}
+    NUMBER=$((10#${BASH_REMATCH[2]})) # Convert to base 10 to drop leading zeroes.
     PROBLEM_NAME=${BASH_REMATCH[3]}
     EXTENSION=${BASH_REMATCH[4]}
     LANGUAGE_ICON=$(get_language_icon_with_link "$EXTENSION" "$file")
