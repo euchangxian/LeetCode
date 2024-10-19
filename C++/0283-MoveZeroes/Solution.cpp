330f5038cb9e59260eee3397824bd5a7a0c9ffd1
euchangxian <changxian2001@gmail.com>
2024-10-20 00:59:36 +0800

    feat(C++): Add 283

---
+++ C++/0283-MoveZeroes/Solution.cpp
@@ -0,0 +1,22 @@
+#include <cstddef>
+#include <vector>
+
+using namespace std;
+class Solution {
+ public:
+  void moveZeroes(vector<int>& nums) {
+    // Seems like two pointer where the first k elements are non-zeros
+    // Similar to Remove Duplicates from Sorted array, where the relative order
+    // of the elements must be maintained.
+    size_t insertIdx = 0;
+    for (size_t i = 0; i < nums.size(); ++i) {
+      if (nums[i] == 0) {
+        continue;
+      }
+      const int num = nums[i];
+      nums[i] = 0;  // zero out the original. This enables a single traversal
+      nums[insertIdx] = num;
+      ++insertIdx;
+    }
+  }
+};
