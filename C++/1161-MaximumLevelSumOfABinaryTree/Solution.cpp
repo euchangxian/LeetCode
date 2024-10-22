381b9fddddc4279936ba8e6c49528a1d5fe57156
euchangxian <e0957388@u.nus.edu>
2024-10-22 11:47:28 +0800

    feat(C++): Add 1161

---
+++ C++/1161-MaximumLevelSumOfABinaryTree/Solution.cpp
@@ -0,0 +1,53 @@
+#include <algorithm>
+#include <cstddef>
+#include <queue>
+
+struct TreeNode {
+  int val;
+  TreeNode* left;
+  TreeNode* right;
+
+  TreeNode() : val(0), left(nullptr), right(nullptr) {}
+  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+  TreeNode(int x, TreeNode* left, TreeNode* right)
+      : val(x), left(left), right(right) {}
+};
+
+using namespace std;
+class Solution {
+ public:
+  int maxLevelSum(TreeNode* root) {
+    int maxLevelSum = INT_MIN;
+    int maxLevel = 0;
+
+    // Do a level order traversal
+    std::queue<TreeNode*> frontier;
+    frontier.push(root);
+    int level = 0;
+    while (!frontier.empty()) {
+      size_t fSize = frontier.size();
+      int levelSum = 0;
+      while (fSize--) {
+        TreeNode* curr = frontier.front();
+        frontier.pop();
+
+        levelSum += curr->val;
+        if (curr->left) {
+          frontier.push(curr->left);
+        }
+
+        if (curr->right) {
+          frontier.push(curr->right);
+        }
+      }
+      ++level;
+
+      if (levelSum > maxLevelSum) {
+        maxLevelSum = levelSum;
+        maxLevel = level;
+      }
+    }
+
+    return maxLevel;
+  }
+};
