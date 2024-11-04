5d461d3a2ab37eda1134506a9fcd28e955b1bb15
euchangxian <changxian2001@gmail.com>
2024-11-04 08:42:20 +0800

    feat(C++): Add 443

---
+++ C++/0443-StringCompression/Solution.cpp
@@ -0,0 +1,42 @@
+#include <cstddef>
+#include <string>
+#include <vector>
+
+using namespace std;
+class Solution {
+ public:
+  int compress(std::vector<char>& chars) {
+    // s := ""
+    // for each group of consecutive characters in chars:
+    //   if group.length == 1:
+    //     append c to s
+    //   else:
+    //     append c, followed by group.length
+    // return s.length
+    // MODIFY the input array and return its new length.
+    int len = 0;  // represents the writeIdx
+    int i = 0;
+    while (i < chars.size()) {
+      int repeats = 0;
+      char curr = chars[i];
+
+      while (i < chars.size() && chars[i] == curr) {
+        ++i;
+        ++repeats;
+      }
+
+      // modify the input array
+      chars[len++] = curr;
+      if (repeats > 1) {
+        // repeats <= 2000
+        // Can we do better than converting to a string?
+        std::string digits = std::to_string(repeats);
+        for (char d : digits) {
+          chars[len++] = d;
+        }
+      }
+    }
+
+    return len;
+  }
+};
