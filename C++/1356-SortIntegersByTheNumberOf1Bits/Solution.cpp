1c352566e1c22e2c6a01f6f0dd6b352e2e92a16f
euchangxian <changxian2001@gmail.com>
2024-11-06 14:10:10 +0800

    feat(sorting): Add 1356

---
+++ C++/1356-SortIntegersByTheNumberOf1Bits/Solution.cpp
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <vector>
+
+using namespace std;
+class Solution {
+ public:
+  std::vector<int> sortByBits(std::vector<int>& arr) {
+    std::sort(arr.begin(), arr.end(), [](int a, int b) {
+      // For a comparator comp(a, b), it must define a strict weak ordering.
+      // That is, it must satisfy these properties:
+      // 1. Irreflexivity: comp(x, x) == false
+      // 2. Asymmetry: if comp(x, y) == true, then comp(y, x) == false, and vice
+      //               versa.
+      // 3. Transitivity: If comp(x, y) == true && comp(y, z) == true then
+      //                  comp(x, z) = true.
+      return __builtin_popcount(a) < __builtin_popcount(b) ||
+             (__builtin_popcount(a) == __builtin_popcount(b) && a < b);
+    });
+    return arr;
+  }
+};
