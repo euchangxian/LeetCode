1c5759ca4804c4a4a09b1991a575c447f6797fa3
euchangxian <changxian2001@gmail.com>
2024-11-25 23:10:42 +0800

    chore: Rewrite 252 for LeetCode submission

--- C++/0252-MeetingRooms/Solution.cpp
+++ C++/0252-MeetingRooms/Solution.cpp
@@ -1,29 +1,15 @@
 #include <algorithm>
-#include <climits>
-#include <functional>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
-class Interval {
- public:
-  int start, end;
-  Interval(int start, int end) : start(start), end(end) {}
-};
 
-using namespace std;
 class Solution {
  public:
-  bool canAttendMeetings(vector<Interval>& intervals) {
-    sort(
-        intervals.begin(), intervals.end(),
-        [](Interval const& a, Interval const& b) { return a.start < b.start; });
+  bool canAttendMeetings(std::vector<std::vector<int>>& intervals) {
+    std::sort(intervals.begin(), intervals.end());
 
+    // check for overlaps, i.e., if the previous meeting's ending cuts into the
+    // current meeting.
     for (int i = 1; i < intervals.size(); ++i) {
-      if (intervals[i].start < intervals[i - 1].end) {
+      if (intervals[i - 1][1] > intervals[i][0]) {
         return false;
       }
     }
