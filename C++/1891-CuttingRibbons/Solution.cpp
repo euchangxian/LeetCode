1594d2e24c1145a2055753f33dcfc0b2e6d695fe
euchangxian <changxian2001@gmail.com>
2024-12-14 01:25:11 +0800

    feat(binary-search): Add 1891

---
+++ C++/1891-CuttingRibbons/Solution.cpp
@@ -0,0 +1,33 @@
+#include <algorithm>
+#include <cstddef>
+#include <cstdlib>
+#include <vector>
+
+class Solution {
+ private:
+  bool feasible(std::vector<int>& ribbons, int k, int len) {
+    for (int ribbon : ribbons) {
+      k -= (ribbon / len);
+    }
+    return k <= 0;
+  }
+
+ public:
+  int maxLength(std::vector<int>& ribbons, int k) {
+    // binary search probably, where we want to seach for maxLength.
+    int left = 0;
+    int right = *std::max_element(ribbons.begin(), ribbons.end());
+
+    while (left < right) {
+      int mid = left + (right - left + 1) / 2;
+
+      if (feasible(ribbons, k, mid)) {
+        // try a bigger cut
+        left = mid;
+      } else {
+        right = mid - 1;
+      }
+    }
+    return left;
+  }
+};
