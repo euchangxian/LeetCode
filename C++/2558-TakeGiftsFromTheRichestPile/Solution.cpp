55e3789c2c696f432d62b7d613160905b01efb27
euchangxian <changxian2001@gmail.com>
2024-12-14 01:18:35 +0800

    feat(heap): Add 2558
    honestly still cant internalize default cmp of heaps

---
+++ C++/2558-TakeGiftsFromTheRichestPile/Solution.cpp
@@ -0,0 +1,22 @@
+#include <algorithm>
+#include <cmath>
+#include <cstddef>
+#include <cstdlib>
+#include <functional>
+#include <vector>
+
+class Solution {
+ public:
+  long long pickGifts(std::vector<int>& gifts, int k) {
+    // max heap
+    std::make_heap(gifts.begin(), gifts.end());
+
+    while (k--) {
+      int leave = std::sqrt(gifts[0]);
+      std::pop_heap(gifts.begin(), gifts.end());
+      gifts.back() = leave;
+    }
+
+    return std::reduce(gifts.begin(), gifts.end(), 0LL, std::plus<>{});
+  }
+};
