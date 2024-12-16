d67f46bb706a29837cadc2d1b2e16c5f56764700
euchangxian <changxian2001@gmail.com>
2024-12-16 17:08:40 +0800

    feat(stack): Add 2832

---
+++ C++/2832-MaximalRangeThatEachElementIsMaximumInIt/Solution.cpp
@@ -0,0 +1,40 @@
+#include <cstddef>
+#include <cstdlib>
+#include <stack>
+#include <vector>
+
+class Solution {
+ public:
+  std::vector<int> maximumLengthOfRanges(std::vector<int>& nums) {
+    // seems like monotonic-decreasing stack to me.
+    // When popping from the stack, i.e., a larger element at index j is seen,
+    // the length of the range can be calculated.
+    const size_t n = nums.size();
+    std::vector<int> lengths(n, 0);
+    std::stack<int> maxWindow;
+    for (int i = 0; i < n; ++i) {
+      // maintain decreasing property:
+      while (!maxWindow.empty() && nums[i] > nums[maxWindow.top()]) {
+        int top = maxWindow.top();
+        maxWindow.pop();
+
+        // if empty, that means that the element is the biggest element of the
+        // prefix [0..j)
+        int prev = maxWindow.empty() ? -1 : maxWindow.top();
+        lengths[top] = i - prev - 1;
+      }
+
+      maxWindow.push(i);
+    }
+
+    // handle decreasing subarrays that end at the last element.
+    while (!maxWindow.empty()) {
+      int top = maxWindow.top();
+      maxWindow.pop();
+
+      int prev = maxWindow.empty() ? -1 : maxWindow.top();
+      lengths[top] = n - prev - 1;
+    }
+    return lengths;
+  }
+};
