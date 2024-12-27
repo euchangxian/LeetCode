908b09df07291adeddec7396282ed9144d4e3c19
euchangxian <changxian2001@gmail.com>
2024-12-27 14:05:29 +0800

    feat(dp): Add 1014

--- C++/1014-BestSightseeingPair/Solution.cpp
+++ C++/1014-BestSightseeingPair/Solution.cpp
@@ -1,34 +1,22 @@
 #include <algorithm>
-#include <climits>
-#include <functional>
-#include <iostream>
-#include <queue>
-#include <stack>
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
 #include <vector>
 
-using namespace std;
 class Solution {
  public:
-  int maxScoreSightseeingPair(vector<int>& values) {
-    int n = values.size();
+  int maxScoreSightseeingPair(std::vector<int>& values) {
+    // Score of a pair (i, j), i < j is values[i] + values[j] + i - j.
+    // i.e., sum of values - distance.
+    // Hmmm. We know for sure that as we iterate through j, the closest maximum
+    // is more optimal. But its value decreases by 1 as we iterate.
+    // Ooo. Similar to Kadane's
+    int maxScore = 0;
+    int maxVal = 0;
 
-    // Maintain the maximum score pair so-far
-    int result = 0;
-
-    // Maintain the maximum value to the left, normalized by
-    // the distance from the current index i
-    int prefixMax = values[0];
-
-    for (int i = 1; i < n; ++i) {
-      result = max(result, prefixMax - 1 + values[i]);
-
-      // Update prefixMax by checking if taking a previous value
-      // is worth the distance penalty, or simply take the current value
-      prefixMax = max(prefixMax - 1, values[i]);
+    for (int num : values) {
+      maxScore = std::max(maxScore, maxVal + num - 1);
+      maxVal = std::max(maxVal - 1, num);
     }
-    return result;
+
+    return maxScore;
   }
 };
