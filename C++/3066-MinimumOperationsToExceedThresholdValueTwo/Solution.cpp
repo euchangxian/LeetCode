005a9b078b128646223b8d6502e010277131d24a
euchangxian <changxian2001@gmail.com>
2025-02-13 12:15:59 +0800

    refactor(style) 3066

--- C++/3066-MinimumOperationsToExceedThresholdValueTwo/Solution.cpp
+++ C++/3066-MinimumOperationsToExceedThresholdValueTwo/Solution.cpp
@@ -3,10 +3,10 @@
 #include <functional>
 #include <vector>
 
-using namespace std;
+using i64 = long long;
 class Solution {
  public:
-  int minOperations(vector<int>& nums, int k) {
+  int minOperations(std::vector<int>& nums, int k) {
     // Problem constraints guarantee that an answer always exists. No need for
     // input validation.
     //
@@ -14,30 +14,30 @@ class Solution {
     // efficiently.
     // Repeat until x, y is greater than k, as both x, y are the 2 smallest
     // elements.
-    const size_t n = nums.size();
-    vector<long long> llNums(n);
-    for (size_t i = 0; i < n; ++i) {
-      llNums[i] = static_cast<long long>(nums[i]);
+    const std::size_t n = nums.size();
+    std::vector<i64> llNums(n);
+    for (std::size_t i = 0; i < n; ++i) {
+      llNums[i] = static_cast<i64>(nums[i]);
     }
 
     // Use a minHeap.
-    const auto cmp = greater<>{};
+    const auto cmp = std::greater<>{};
     std::make_heap(llNums.begin(), llNums.end(), cmp);
 
     int operations = 0;
     while (llNums.size() >= 2) {
-      const long long x = llNums.front();
+      const i64 x = llNums.front();
       std::pop_heap(llNums.begin(), llNums.end(), cmp);
       llNums.pop_back();  // actually pop from the underlying array
 
-      const long long y = llNums.front();
+      const i64 y = llNums.front();
       std::pop_heap(llNums.begin(), llNums.end(), cmp);
 
       if (x >= k && y >= k) {
         break;
       }
 
-      const long long toAdd = std::min(x, y) * 2 + std::max(x, y);
+      const i64 toAdd = std::min(x, y) * 2 + std::max(x, y);
 
       llNums.back() = toAdd;
       std::push_heap(llNums.begin(), llNums.end(), cmp);
