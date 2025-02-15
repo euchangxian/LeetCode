3c192b183e2afd0721a9d0db0227e294d3eb3716
euchangxian <changxian2001@gmail.com>
2025-02-15 16:45:16 +0800

    refactor(style): 1605

--- C++/1605-FindValidMatrixGivenRowAndColumnSums/Solution.cpp
+++ C++/1605-FindValidMatrixGivenRowAndColumnSums/Solution.cpp
@@ -1,17 +1,18 @@
+#include <algorithm>
 #include <vector>
 
-using namespace std;
 class Solution {
  public:
-  vector<vector<int>> restoreMatrix(vector<int>& rowSum, vector<int>& colSum) {
+  std::vector<std::vector<int>> restoreMatrix(std::vector<int>& rowSum,
+                                              std::vector<int>& colSum) {
     int n = rowSum.size();
     int m = colSum.size();
 
-    vector<vector<int>> matrix(n, vector<int>(m, 0));
+    std::vector<std::vector<int>> matrix(n, std::vector<int>(m, 0));
     for (int i = 0; i < n; ++i) {
       for (int j = 0; j < m; ++j) {
         // Greedily take the most
-        int current = min(rowSum[i], colSum[j]);
+        int current = std::min(rowSum[i], colSum[j]);
 
         // update the remaining sum
         rowSum[i] -= current;
