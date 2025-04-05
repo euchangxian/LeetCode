8dbb9c4ae4024e80f7ac2cec1c4a82f8a2662d47
euchangxian <changxian2001@gmail.com>
2025-04-05 21:53:21 +0800

    feat(BitManipulation,XOR): Add 1863

--- C++/1863-SumOfAllSubsetXORTotals/Solution.cpp
+++ C++/1863-SumOfAllSubsetXORTotals/Solution.cpp
@@ -1,8 +1,32 @@
+#include <functional>
+#include <numeric>
 #include <vector>
 
 class Solution {
  public:
   int subsetXORSum(std::vector<int>& nums) {
+    // Notice that of the 2^n subsets, nums[i] will be in exactly half of them.
+    // Let k=2^n.
+    // Then, the bits of nums[i] will be included k/2 times.
+    // Not exactly correct to say this. The bits may be XOR-ed/toggled off/on.
+    // Hm. If the x-th bit is set in nums[i], it must be set in half of the
+    // XOR totals.
+    // If the x-th bit is set in more than one of the elements, it will be set
+    // in half of the XOR totals. More specifically, consider the previous
+    // scenario where half of the subsets have the xth bit set, and the other
+    // half do not.
+    // Then, the on-subset will be toggled to off, and vice-versa, remaining
+    // at half.
+    // Thus, we can take the bitwise OR of every element to get all x_i bits,
+    // then add them to the sum k/2 = 2^(n-1) = (1 << (n-1)) times.
+    // Wild.
+    const int n = nums.size();
+    return std::reduce(nums.begin(), nums.end(), 0, std::bit_or<>{}) << (n - 1);
+  }
+
+  // Lol. This is worse than expected.
+  // Simply dfs(nums, i, total) is sufficient.
+  int subsetXORSumBacktracking(std::vector<int>& nums) {
     std::vector<std::vector<int>> subsets = generateAllSubsets(nums);
     int xorSum = 0;
 
