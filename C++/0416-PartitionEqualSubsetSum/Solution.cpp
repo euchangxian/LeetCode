de2ad1f52df7ab026259e879099804f66c808281
euchangxian <changxian2001@gmail.com>
2025-04-07 11:28:23 +0800

    feat(DP): Refine 416
    Add top-down approach

--- C++/0416-PartitionEqualSubsetSum/Solution.cpp
+++ C++/0416-PartitionEqualSubsetSum/Solution.cpp
@@ -1,7 +1,10 @@
+#include <bitset>
 #include <functional>
 #include <numeric>
 #include <vector>
 
+constexpr int MAX_N = 200;
+constexpr int MAX_VAL = 100;
 class Solution {
  public:
   bool canPartition(std::vector<int>& nums) {
@@ -13,29 +16,64 @@ class Solution {
     // Possibly formulate it as dp[i], 1 <= 2 * k, dp[i] represents whether
     // it is possible to create a subset of sum i
 
-    const int32_t sum = std::reduce(nums.begin(), nums.end(), 0, std::plus<>{});
+    const int sum = std::reduce(nums.begin(), nums.end(), 0, std::plus<>{});
 
     // if the sum is odd, then there is no way to partition into two equal
     // subsets
-    if (sum & 1) {
+    if (sum % 2 != 0) {
       return false;
     }
+    const int k = sum / 2;
 
-    const int32_t k = sum / 2;
-    std::vector<bool> dp(k + 1, false);
+    std::bitset<MAX_N * MAX_VAL + 1> dp{};
     dp[0] = true;
-
     // Similar to coin change. BUT, iterate over the coins in the outer loop
     // to ensure each coin is taken at MOST once.
-    for (const int num : nums) {
+    for (int num : nums) {
       // IMPORTANT: we iterate backwards so that results from earlier iteration
       // will not affect the current iteration. Emulates a temporary array.
       // E.g., if num=1, and we iterate from the start, its akin to taking
       // multiple copies of 1.
-      for (std::size_t amt = k; amt >= num; --amt) {
-        dp[amt] = dp[amt] || dp[amt - num];
+      for (int amt = k; amt >= num; --amt) {
+        dp[amt] = dp[amt] | dp[amt - num];
       }
     }
     return dp[k];
+  }
+
+  bool canPartitionMemo(std::vector<int>& nums) {
+    // dfs(i, sum) =
+    //    dfs(i+1, sum) // skip
+    // || dfs(i+1, sum-nums[i]) // take
+    const int n = nums.size();
+    int sum = std::reduce(nums.begin(), nums.end(), 0, std::plus<>{});
+    if (sum % 2 != 0) {
+      return false;
+    }
+
+    int k = sum / 2;
+    std::vector<std::vector<int>> memo(n, std::vector<int>(k + 1, -1));
+    return dfs(nums, memo, 0, k);
+  }
+
+  bool dfs(std::vector<int>& nums,
+           std::vector<std::vector<int>>& memo,
+           int i,
+           int k) {
+    if (k <= 0) {
+      return k == 0;
+    }
+
+    if (i >= nums.size()) {
+      return false;
+    }
+
+    int& ans = memo[i][k];
+    if (ans != -1) {
+      return ans;
+    }
+
+    return ans =
+               dfs(nums, memo, i + 1, k) || dfs(nums, memo, i + 1, k - nums[i]);
   }
 };
