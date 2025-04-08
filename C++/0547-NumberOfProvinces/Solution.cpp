87b07d5492fe558037c8b98f991bed84ea61f594
euchangxian <changxian2001@gmail.com>
2025-04-08 10:55:51 +0800

    feat(UnionFind): Add 547

---
+++ C++/0547-NumberOfProvinces/Solution.cpp
@@ -0,0 +1,98 @@
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <cmath>
+#include <cstdint>
+#include <deque>
+#include <functional>
+#include <iostream>
+#include <istream>
+#include <iterator>
+#include <limits>
+#include <map>
+#include <numeric>
+#include <queue>
+#include <ranges>
+#include <set>
+#include <sstream>
+#include <stack>
+#include <string>
+#include <string_view>
+#include <tuple>
+#include <unordered_map>
+#include <unordered_set>
+#include <utility>
+#include <vector>
+
+class UnionFind {
+ public:
+  UnionFind(int n) : components_(n), parent_(n), rank_(n, 0), size_(n, 1) {
+    for (int i = 0; i < n; ++i) {
+      parent_[i] = i;
+    }
+  }
+
+  int find(int x) {
+    if (parent_[x] != x) {
+      parent_[x] = find(parent_[x]);
+    }
+
+    return parent_[x];
+  }
+
+  bool connected(int x, int y) { return find(x) == find(y); }
+
+  void unite(int x, int y) {
+    int rootX = find(x);
+    int rootY = find(y);
+
+    if (rootX == rootY) {
+      return;
+    }
+
+    --components_;
+    if (rank_[rootX] < rank_[rootY]) {
+      parent_[rootX] = rootY;
+      size_[rootY] += std::exchange(size_[rootX], 0);
+      return;
+    }
+
+    if (rank_[rootX] == rank_[rootY]) {
+      ++rank_[rootX];
+    }
+    parent_[rootY] = rootX;
+    size_[rootX] += std::exchange(size_[rootY], 0);
+  }
+
+  int components() const noexcept { return components_; }
+
+  int size(int x) { return size_[find(x)]; }
+
+ private:
+  int components_;
+
+  std::vector<int> parent_;
+  std::vector<int> rank_;
+  std::vector<int> size_;
+};
+
+class Solution {
+ public:
+  int findCircleNum(std::vector<std::vector<int>>& isConnected) {
+    // A province is a group of directly/indirectly connected cities.
+    const int n = isConnected.size();
+    UnionFind G(n);
+
+    for (int i = 0; i < n; ++i) {
+      // Edges are bidirectional.
+      for (int j = i + 1; j < n; ++j) {
+        if (isConnected[i][j]) {
+          G.unite(i, j);
+        }
+      }
+    }
+
+    return G.components();
+  }
+};
