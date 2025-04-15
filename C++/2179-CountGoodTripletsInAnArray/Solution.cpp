ee94a481c728089682057829e911df30704c2c3b
euchangxian <changxian2001@gmail.com>
2025-04-15 19:29:57 +0800

    feat(FenwickTree): Refactor 2179
    The optimized version is quite unintuitive. Should revisit

--- C++/2179-CountGoodTripletsInAnArray/Solution.cpp
+++ C++/2179-CountGoodTripletsInAnArray/Solution.cpp
@@ -4,11 +4,21 @@
 #include <vector>
 
 using i64 = long long;
+constexpr int MAX_N = 1e5;
+
 class FenwickTree {
  public:
   FenwickTree(int n) : N(n), tree(N + 1, 0) {}
 
-  int query(int l, int r) const { return query_(r) - query_(l - 1); }
+  int query(int l, int r) const { return prefix(r) - prefix(l - 1); }
+
+  int prefix(int i) const {
+    int sum = 0;
+    for (; i > 0; i -= i & -i) {
+      sum += tree[i];
+    }
+    return sum;
+  }
 
   void update(int i, int diff) {
     for (; i <= N; i += i & -i) {
@@ -21,19 +31,48 @@ class FenwickTree {
  private:
   const int N;
   std::vector<int> tree;
-
-  int query_(int i) const {
-    int sum = 0;
-    for (; i > 0; i -= i & -i) {
-      sum += tree[i];
-    }
-    return sum;
-  }
 };
 
 class Solution {
  public:
   i64 goodTriplets(std::vector<int>& nums1, std::vector<int>& nums2) {
+    const int n = nums1.size();
+
+    std::vector<int> pos2(n);
+    for (int i = 0; i < n; ++i) {
+      pos2[nums2[i]] = i + 1;  // 1-indexed for more intuitive FT queries.
+    }
+
+    // Keyed by value.
+    FenwickTree ft(n);
+    i64 result = 0;
+
+    // To optimize to 1-pass, the key insight is to iterate over nums1[j] as
+    // the middle element.
+    // Count the number of elements
+    for (int j = 0; j < n; ++j) {
+      int y = pos2[nums1[j]];
+
+      // Query x < y in nums2
+      i64 left = ft.prefix(y - 1);
+
+      // We want z > y, while also ensuring k > j
+      // Given that we have n distinct elements, we take n-y to get the number
+      // of elements greater than y. This fulfils z > y
+      // Then, with j-left giving us the number of elements in nums1 that are
+      // already processed, and to the right of y.
+      // we can do (n-y) - (j-left) to get the number of elements to the right
+      // of y, that have not yet been processed, i.e., to the right of j in
+      // nums1, fulfilling k > j.
+      i64 right = (n - y) - (j - left);
+      result += left * right;
+
+      ft.update(y, 1);
+    }
+    return result;
+  }
+
+  i64 goodTripletsNaive(std::vector<int>& nums1, std::vector<int>& nums2) {
     // both nums1 and nums2 are permutations of [0..n-1].
     // A good triplet is a set of 3 distinct values present in both
     // nums1 and nums2.
