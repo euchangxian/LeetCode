27ee514fd0732155674b916b676cddda6475ba1c
euchangxian <changxian2001@gmail.com>
2025-09-01 19:08:15 +0800

    feat(Heap): Add 1792

--- C++/1792-MaximumAveragePassRatio/Solution.cpp
+++ C++/1792-MaximumAveragePassRatio/Solution.cpp
@@ -1,6 +1,7 @@
 #include <algorithm>
 #include <cstddef>
 #include <cstdlib>
+#include <queue>
 #include <utility>
 #include <vector>
 
@@ -8,6 +9,69 @@ class Solution {
  public:
   double maxAverageRatio(std::vector<std::vector<int>>& classes,
                          int extraStudents) {
+    auto calcIncrement = [](const auto& clazz) {
+      double pass = clazz[0];
+      double total = clazz[1];
+      return ((pass + 1) / (total + 1)) - ((pass) / total);
+    };
+
+    const auto n = static_cast<int>(classes.size());
+
+    using Node = std::pair<double, int>;
+    std::priority_queue<Node> maxHeap;
+    for (int i = 0; i < n; ++i) {
+      maxHeap.emplace(calcIncrement(classes[i]), i);
+    }
+
+    while (extraStudents--) {
+      auto [_, idx] = maxHeap.top();
+      maxHeap.pop();
+
+      auto& clazz = classes[idx];
+      ++clazz[0];
+      ++clazz[1];
+      maxHeap.emplace(calcIncrement(clazz), idx);
+    }
+
+    double sumPassRate = 0.0;
+    for (const auto& clazz : classes) {
+      double pass = clazz[0];
+      double total = clazz[1];
+      sumPassRate += pass / total;
+    }
+
+    return sumPassRate / classes.size();
+  }
+
+  double maxAverageRatioRangesHeap(std::vector<std::vector<int>>& classes,
+                                   int extraStudents) {
+    auto calcIncrement = [](const auto& clazz) {
+      double pass = clazz[0];
+      double total = clazz[1];
+      return ((pass + 1) / (total + 1)) - ((pass) / total);
+    };
+
+    std::ranges::make_heap(classes, {}, calcIncrement);
+    while (extraStudents--) {
+      std::ranges::pop_heap(classes, {}, calcIncrement);
+      auto& clazz = classes.back();
+      ++clazz[0];
+      ++clazz[1];
+      std::ranges::push_heap(classes, {}, calcIncrement);
+    }
+
+    double sumPassRate = 0.0;
+    for (const auto& clazz : classes) {
+      double pass = clazz[0];
+      double total = clazz[1];
+      sumPassRate += pass / total;
+    }
+
+    return sumPassRate / classes.size();
+  }
+
+  double maxAverageRatioVec(std::vector<std::vector<int>>& classes,
+                            int extraStudents) {
     // first, notice that the smaller the total, the more the extraStudents
     // contribute to the passing rate.
     // second, notice that the smaller the number of students that pass, the
@@ -19,8 +83,8 @@ class Solution {
     // classes = [[1,2], [1, 100000]], extraStudents = 1, or
     // classes = [[1,2], [99'999, 100'000]], extraStudents = 1.
     //
-    // The increment in the passing ratio is just one more layer of indirection:
-    // new_pass_ratio - original_pass_ratio.
+    // The increment in the passing ratio is just one more layer of
+    // indirection: new_pass_ratio - original_pass_ratio.
     //
     // Seems like a maxHeap kinda solution.
     auto calcIncr = [](double pass, double total) -> double {
