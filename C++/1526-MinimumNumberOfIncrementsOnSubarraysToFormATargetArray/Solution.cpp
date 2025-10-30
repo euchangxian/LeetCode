b79e14f6e8e3d0fad1765159efb155ce67bf10af
euchangxian <changxian2001@gmail.com>
2025-10-30 14:27:10 +0800

    feat(Greedy): Add 1526

--- C++/1526-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/Solution.cpp
+++ C++/1526-MinimumNumberOfIncrementsOnSubarraysToFormATargetArray/Solution.cpp
@@ -1,4 +1,5 @@
 #include <algorithm>
+#include <cstddef>
 #include <vector>
 
 class Solution {
@@ -9,9 +10,8 @@ class Solution {
     // i.e., {1, 2, 3} => 3 operations required.
     // {3, 1, 2} => 4 operations required
     // Quite similar to counting the left edges that are exposed
-
     int count{target[0]};
-    for (size_t i{1}; i < target.size(); ++i) {
+    for (std::size_t i{1}; i < target.size(); ++i) {
       count += std::max(target[i] - target[i - 1], 0);
     }
     return count;
